1cdeba8db839ea6e13ab8050d1a9ae10d1e11d54
nocheacosador <lukaszaj@gmail.com>
2022-09-20 05:28:37 +0300

    Added form validation, changed method signatures.

--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Button, Input, Label } from "reactstrap";
 import { authenticationService } from './auth/AuthenticationServce';
@@ -7,6 +7,27 @@ import { NamedLogo } from "./NamedLogo";
 function Register() {
     const userLoggedIn = authenticationService.userIsLoggedIn();
     const navigate = useNavigate();
+
+    // { id: elementId, msg: error_message }
+    const [error, setError] = useState(null);
+
+    function getUserInput() {
+        return {
+            username: document.getElementById("username").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            repeatPassword: document.getElementById("repeatPassword").value
+        }
+    }
+
+    function validateUserInput(input) {
+        if (input.username === "") return { ok: false, err: { id: "username", msg: "Enter username" }};
+        if (input.email === "")  return { ok: false, err: { id: "email", msg: "Enter email" }};
+        if (input.password === "")  return { ok: false, err: { id: "password", msg: "Enter password" }};
+        if (input.repeatPassword === "")  return { ok: false, err: { id: "repeatPassword", msg: "Repeat password" }};
+        if (input.password !== input.repeatPassword)  return { ok: false, err: { id: "repeatPassword", msg: "Passwords don't match" }};
+        return { ok: true };
+    }
 
     return (
         <>
@@ -20,25 +41,47 @@ function Register() {
                                     <h3 className="text-primary mb-0"><NamedLogo/></h3>
                                 </Link>
                             </div>
+                            { (error !== null && error.id === "username" ) &&
+                            <div className="text-end" style={{color: "var(--primary)"}}><p>{error.msg}</p></div> }
                             <div className="form-floating mb-3">
-                                <Input type="text" className="form-control" placeholder="Username"/>
+                                <Input type="text" className="form-control" placeholder="Username" id="username"/>
                                 <Label>Username</Label>
                             </div>
+                            { (error !== null && error.id === "email" ) &&
+                            <div className="text-end" style={{color: "var(--primary)"}}><p>{error.msg}</p></div> }
                             <div className="form-floating mb-3">
-                                <Input type="email" className="form-control" placeholder="name@example.com"/>
+                                <Input type="email" className="form-control" placeholder="name@example.com" id="email"/>
                                 <Label>Email address</Label>
                             </div>
+                            { (error !== null && error.id === "password" ) &&
+                            <div className="text-end" style={{color: "var(--primary)"}}><p>{error.msg}</p></div> }
                             <div className="form-floating mb-3">
-                                <Input type="password" className="form-control" placeholder="Password"/>
+                                <Input type="password" className="form-control" placeholder="Password" id="password"/>
                                 <Label>Password</Label>
                             </div>
+                            { (error !== null && error.id === "repeatPassword" ) &&
+                            <div className="text-end" style={{color: "var(--primary)"}}><p>{error.msg}</p></div> }
                             <div className="form-floating mb-4">
-                                <Input type="password" className="form-control" placeholder="Password"/>
+                                <Input type="password" className="form-control" placeholder="Password" id="repeatPassword"/>
                                 <Label>Repeat password</Label>
                             </div>
                             <Button color="primary" className="py-3 w-100 mb-4" onClick={() => {
-                                authenticationService.logIn();
-                                navigate('/');
+                                let userData = getUserInput();
+                                let valid = validateUserInput(userData);
+                                if (valid.ok) {
+                                    setError(null);
+                                    authenticationService.register({
+                                        name: userData.username,
+                                        email: userData.email,
+                                        password: userData.password
+                                    }).then((result) => console.log(result));
+                                }
+                                else {
+                                    setError(valid.err);
+                                }
+                                
+                                //authenticationService.logIn();
+                                //navigate('/');
                             }}>Register</Button>
                             <p className="text-center mb-0">
                                 Already have an Account? <Link to="/login">Log In</Link>
