02560dcc938a6e0e9ea915fc929f2f52a87974ab
Danielius Semenas <d_semenas@Danieliuss-Air.lan>
2022-09-27 01:46:52 +0300

    Added product controller.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartShopping.Dtos;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartShopping.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        // GET: /<controller>/
+        [HttpPost("submit")]
+        public IActionResult Submit ([FromBody] ProductList products)
+        {
+            foreach(var p in products.Products)
+            {
+                if(p.ProductName == null || p.Shop == null || p.Price == null || p.DateOfPurchase == null)
+                {
+                    return BadRequest("All fields should be filled");
+                }
+                else
+                {
+                    return Ok($"Product/Products is/are createds");
+                }
+            }
+            return BadRequest();
+            
+        }
+    }
+}
+
---
+++ Dtos/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SmartShopping.Dtos
+{
+    public class ProductDto
+    {
+        public string? ProductName { get; set; }
+        public double Price { get; set; }
+        public string? Shop { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+    }
+        public class ProductList
+        {
+           public List<ProductDto>? Products { get; set; }
+        }
+ }
+
