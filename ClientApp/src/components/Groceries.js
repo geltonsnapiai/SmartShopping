f5a6a7083f93814c6cf7a8d99a7c9ee7e1aeee6c
rytis17 <tritis02@gmail.com>
2022-09-26 23:13:07 +0300

    Added new tab where you can add groceries

--- ClientApp/src/components/Groceries.js
+++ ClientApp/src/components/Groceries.js
@@ -1,13 +1,65 @@
 ﻿import React, { Component } from 'react';
 
 export class Groceries extends Component {
-    static displayName = Groceries.name;
-
-    render() {
-        return (
-            <div>
-                <h1>Groceries</h1>
-                </div>
-        );
+    constructor() {
+        super();
+        this.state = {
+            groceries: []
+        }
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let groceries = this.state.groceries;
+        let storeName = this.refs.storeName.value;
+        let itemName = this.refs.itemName.value;
+        let itemPrice = this.refs.itemPrice.value;
+
+        let newGroceries = {
+            "storeName" : storeName,
+            "itemName" : itemName,
+            "itemPrice" : itemPrice,
+        }
+        groceries.push(newGroceries);
+
+        this.setState({
+            groceries : groceries
+        })
+
+        this.refs.groceriesForm.reset();
+    }
+
+    render(){
+        let groceries = this.state.groceries;
+        return(
+            <div>
+                <form ref="groceriesForm">
+                <label>Store Name</label>
+                <input type="text" ref="storeName"/>
+                <label>Item Name</label>
+                <input type="text" ref="itemName"/>
+                <label>Item Price</label>
+                <input type="number" ref="itemPrice"/>
+                <button onClick={e => this.handleSubmit(e)}>Save</button>
+            </form>
+            <table width="500">
+                <tr>
+                    <th>Store Name</th>
+                    <th>Item Name</th>
+                    <th>Item Price</th>
+                </tr>
+                {
+                    groceries.map( (data, i) => 
+                    <tr key={i}>
+                        <td>{data.storeName}</td>
+                        <td>{data.itemName}</td>
+                        <td>{data.itemPrice}</td>
+                    </tr> )
+                }
+            </table>
+            </div>
+        )
+    }
+
+
 }
