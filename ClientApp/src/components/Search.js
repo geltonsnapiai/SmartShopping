51419143c6077f8f9de03a52df1f9e478a904a04
Danielius Semenas <d_semenas@Danieliuss-Air.lan>
2022-10-04 01:30:53 +0300

    changed design

--- ClientApp/src/components/Search.js
+++ ClientApp/src/components/Search.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar/SearchBar';
+import SearchBar from './SearchBar';
+import ProductsBox from './ProductsBox'
 
 export class Search extends Component {
 
     render() {
         return(
-            <div>
+            <div className="container-fluid pt-4 px-4">
+
                 <div> 
                     <SearchBar placeHolder="Enter product"/>    
+                </div>
+
+                <div>
+                    <ProductsBox/>
                 </div>
                
             </div>
