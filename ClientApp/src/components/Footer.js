d708f1dc1ff32017a538c423d640f9fa611f6ef8
nocheacosador <lukaszaj@gmail.com>
2022-11-15 02:04:46 +0200

    Changed link

--- ClientApp/src/components/Footer.js
+++ ClientApp/src/components/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
             <div className="bg-secondary rounded-top p-4">
                 <div className="row">
                     <div className="col-12 col-sm-6 text-center text-sm-start">
-                        &copy; <a href="#">Your Site Name</a>, All Right Reserved. 
+                        &copy; <a href="#">SmartShopping</a>, All Right Reserved. 
                     </div>
                     <div className="col-12 col-sm-6 text-center text-sm-end">
                         Designed By <a href="https://htmlcodex.com">HTML Codex</a>
