ea1b93ef6c650decb55d2343c05aff810c694b75
nocheacosador <lukaszaj@gmail.com>
2022-11-18 16:41:12 +0200

    UI tidy-up

--- ClientApp/src/components/Home/Home.js
+++ ClientApp/src/components/Home/Home.js
@@ -6,8 +6,8 @@ export const Home = () => {
         <div className="container-fluid pt-4 px-4">
             <div className="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
                 <div className="col-md-6">
-                    <div className="home">
-                        <h1>Welcome!</h1>
+                    <div className="home fs-4">
+                        <h1 className="fs-1">Welcome!</h1>
                         <p>Welcome to <strong>SmartShopping</strong> - app that allows you to shop in a smarter way.</p>
                         <p>Currently under <i>'heavy'</i> developement. Hopefully that will change soon.</p>
                     </div>
---
+++ ClientApp/src/components/ProfileButton/ProfileButton.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { authenticationService } from '../../auth/AuthenticationServce';
+
+
+export function ProfileButton({ userName }) {
+    
+    const [state, setstate] = useState(false);
+    
+    const showDropDown=()=> {
+        setstate(true);
+    }
+
+    const hideDropDown=()=> {
+        setstate(false);
+    }
+    
+
+    return(
+        <div className="nav-item dropdown" onMouseEnter={showDropDown} onMouseLeave={hideDropDown}>
+            <a href="#" className="nav-link dropdown-toggle show">
+            <img className="rounded-circle me-lg-2" src="assets/img/avatar.jpg" alt="fa fa-user me-lg-2" style={{width: 40, height: 40}}/>
+                <span className="d-none d-lg-inline-flex">{userName}</span>
+            </a>
+            
+            {state? (<div className="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0 show">
+                {/* <button className="dropdown-item">My Profile</button>
+                <button className="dropdown-item">Settings</button>
+                <button className="dropdown-item">Shopping List</button> */}
+                <button className="dropdown-item" onClick={() => authenticationService.logout()}>Log out</button>
+            </div>)
+            : null}
+            
+        
+        </div>
+    )
+}
