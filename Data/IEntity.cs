ad30aa56ae3abbf3fdadc8d14e760b6f91ad59e2
nocheacosador <lukaszaj@gmail.com>
2022-11-20 18:40:36 +0200

    Created IEntity and INamedEnity interfaces

---
+++ Data/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartShopping.Data
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+
+    public interface INamedEntity : IEntity
+    {
+        string SimplifiedName { get; set; }
+        string DisplayName { get; set; }
+    }
+}
