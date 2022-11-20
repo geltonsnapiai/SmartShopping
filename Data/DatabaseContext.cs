889acaa0a38e599567d50e3df59a5fdeb081b2bf
nocheacosador <lukaszaj@gmail.com>
2022-11-20 18:39:55 +0200

    Refactored DatabaseContext

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -5,44 +5,37 @@ namespace SmartShopping.Data
 {
     public class DatabaseContext : DbContext
     {
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
-        {
-        }
-
-        public DbSet<User> Users { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Shop> Shops { get; set; }
-        public DbSet<PriceRecord> PriceRecords { get; set; }
-        public DbSet<ProductTag> ProductTags { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<User>(entity => {
-                entity.HasIndex(e => e.Id).IsUnique();
-                entity.HasIndex(e => e.Email).IsUnique();
-            });
-            builder.Entity<Product>(entity => {
-                entity.HasIndex(e => e.Id).IsUnique();
-                entity.HasMany(e => e.PriceRecords).WithOne(e => e.Product);
-                entity.HasMany(e => e.Tags).WithMany(e => e.Products);
-                entity.HasMany(e => e.Shops).WithMany(e => e.Products);
-            });
-            builder.Entity<PriceRecord>(entity => {
-                entity.HasIndex(e => e.Id).IsUnique();
-                entity.HasOne(e => e.Shop).WithMany();
-
-                entity.HasOne(e => e.Product).WithMany(e => e.PriceRecords);
-            });
-            builder.Entity<ProductTag>(entity => {
-                entity.HasIndex(e => e.Id).IsUnique();
-
-                entity.HasMany(e => e.Products).WithMany(e => e.Tags);
-            });
-            builder.Entity<Shop>(entity => {
-                entity.HasIndex(e => e.Id).IsUnique();
-
-                entity.HasMany(e => e.Products).WithMany(e => e.Shops);
-            });
+            builder.Entity<IEntity>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
+                })
+                .Entity<INamedEntity>(entity => {
+                    entity.Property(e => e.SimplifiedName).IsRequired();
+                    entity.Property(e => e.DisplayName).IsRequired();
+                }) 
+                .Entity<User>(entity => {
+                    entity.HasIndex(e => e.Email).IsUnique();
+                    entity.Property(e => e.Name).IsRequired();
+                    entity.Property(e => e.PasswordHash).IsRequired();
+                })
+                .Entity<Product>(entity => {
+                    entity.HasMany(e => e.PriceRecords).WithOne(e => e.Product);
+                    entity.HasMany(e => e.Tags).WithMany(e => e.Products);
+                    entity.HasMany(e => e.Shops).WithMany(e => e.Products);
+                })
+                .Entity<PriceRecord>(entity => {
+                    entity.HasOne(e => e.Shop).WithMany();
+                    entity.HasOne(e => e.Product).WithMany(e => e.PriceRecords);
+                })
+                .Entity<ProductTag>(entity => {
+                    entity.HasMany(e => e.Products).WithMany(e => e.Tags);
+                })
+                .Entity<Shop>(entity => {
+                    entity.HasMany(e => e.Products).WithMany(e => e.Shops);
+                });
         }
     }
 }
