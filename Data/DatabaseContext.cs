fd978843a0f63688921fe77a2884818846b9cb37
nocheacosador <lukaszaj@gmail.com>
2022-11-21 00:48:18 +0200

    Deleted interface entities

--- Data/DatabaseContext.cs
+++ Data/DatabaseContext.cs
@@ -9,32 +9,42 @@ namespace SmartShopping.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<IEntity>(entity => {
-                    entity.HasIndex(entity => entity.Id).IsUnique();
-                })
-                .Entity<INamedEntity>(entity => {
-                    entity.Property(e => e.SimplifiedName).IsRequired();
-                    entity.Property(e => e.DisplayName).IsRequired();
-                }) 
+            builder
                 .Entity<User>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
                     entity.HasIndex(e => e.Email).IsUnique();
                     entity.Property(e => e.Name).IsRequired();
                     entity.Property(e => e.PasswordHash).IsRequired();
                 })
                 .Entity<Product>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
                     entity.HasMany(e => e.PriceRecords).WithOne(e => e.Product);
                     entity.HasMany(e => e.Tags).WithMany(e => e.Products);
                     entity.HasMany(e => e.Shops).WithMany(e => e.Products);
+
+
+                    entity.Property(e => e.SimplifiedName).IsRequired();
+                    entity.Property(e => e.DisplayName).IsRequired();
                 })
                 .Entity<PriceRecord>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
                     entity.HasOne(e => e.Shop).WithMany();
                     entity.HasOne(e => e.Product).WithMany(e => e.PriceRecords);
+
+                    entity.Property(e => e.Price).IsRequired();
                 })
                 .Entity<ProductTag>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
                     entity.HasMany(e => e.Products).WithMany(e => e.Tags);
+
+                    entity.Property(e => e.SimplifiedName).IsRequired();
+                    entity.Property(e => e.DisplayName).IsRequired();
                 })
                 .Entity<Shop>(entity => {
+                    entity.HasIndex(entity => entity.Id).IsUnique();
                     entity.HasMany(e => e.Products).WithMany(e => e.Shops);
+
+                    entity.Property(e => e.Name).IsRequired();
                 });
         }
     }
